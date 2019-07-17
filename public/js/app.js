987b56daa23cd7c16ddd7ef0b3e57f437b941aa3
mei-riko <dobrovolska.marina@gmail.com>
2019-07-17 19:17:11 +0300

    check

--- public/js/app.js
+++ public/js/app.js
@@ -76,7 +76,9 @@ var _jquery2 = _interopRequireDefault(_jquery);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-(0, _jquery2.default)(document).ready(function () {});
+(0, _jquery2.default)(document).ready(function () {
+    (0, _jquery2.default)('.slick').slick({});
+});
 
 /***/ }),
 /* 1 */
