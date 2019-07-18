6fea71c32b0de03c2eb42f4090ff0c4b74aade76
mei-riko <dobrovolska.marina@gmail.com>
2019-07-18 16:14:56 +0300

    index page check

--- public/js/app.js
+++ public/js/app.js
@@ -77,7 +77,63 @@ var _jquery2 = _interopRequireDefault(_jquery);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 (0, _jquery2.default)(document).ready(function () {
-    (0, _jquery2.default)('.slick').slick({});
+  // Services Demo
+  (0, _jquery2.default)('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  (0, _jquery2.default)('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    arrows: false,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
+  (0, _jquery2.default)(".content_service__link").on("click", function () {
+    if (!(0, _jquery2.default)(this).hasClass("content_service__link--active")) {
+      (0, _jquery2.default)(".content_service__link.content_service__link--active").removeClass("content_service__link--active");
+      (0, _jquery2.default)(this).addClass("content_service__link--active");
+      var item = (0, _jquery2.default)(this).data("item") - 1;
+      console.log(item);
+      (0, _jquery2.default)(".slider-item[data-slick-index=" + item + "]").click();
+    }
+  });
+
+  // YA Maps
+  ymaps.ready(init);
+
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [55.76, 37.64],
+      zoom: 10
+    }),
+
+
+    // Создаем метку с помощью вспомогательного класса.
+    myPlacemark1 = new ymaps.Placemark([55.8, 37.6], {
+      // Свойства.
+      // Содержимое иконки, балуна и хинта.
+      iconContent: '',
+      balloonContent: 'Адрес',
+      hintContent: 'Стандартный значок метки',
+      iconCaption: 'Офис группы Апекс'
+    }, {
+      // Опции.
+      // Стандартная фиолетовая иконка.
+      preset: 'islands#greenDotIconWithCaption',
+      iconColor: '#329b48'
+    });
+    //myMap.controls.add('smallZoomControl');
+    // Добавляем метки на карту.
+    myMap.geoObjects.add(myPlacemark1);
+  }
 });
 
 /***/ }),
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,59 @@
 import $ from 'jquery'
 $(document).ready(() =>{
-    $('.slick').slick({
+  // Services Demo
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    arrows: false,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
+  $(".content_service__link").on("click", function(){
+    if( !$(this).hasClass("content_service__link--active") ){
+        $(".content_service__link.content_service__link--active").removeClass("content_service__link--active");
+        $(this).addClass("content_service__link--active");
+        let item = $(this).data("item") - 1;
+        console.log(item);
+        $(".slider-item[data-slick-index=" + item + "]").click()
+    }
+  });
+
+  // YA Maps
+  ymaps.ready(init);
+
+  function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10
+    }),
+
+    // Создаем метку с помощью вспомогательного класса.
+    myPlacemark1 = new ymaps.Placemark([55.8, 37.6], {
+      // Свойства.
+      // Содержимое иконки, балуна и хинта.
+      iconContent: '',
+      balloonContent: 'Адрес',
+      hintContent: 'Стандартный значок метки',
+      iconCaption: 'Офис группы Апекс'
+    }, {
+      // Опции.
+      // Стандартная фиолетовая иконка.
+      preset: 'islands#greenDotIconWithCaption',
+      iconColor: '#329b48',
     });
+    //myMap.controls.add('smallZoomControl');
+    // Добавляем метки на карту.
+    myMap.geoObjects.add(myPlacemark1);
+    }
 });
